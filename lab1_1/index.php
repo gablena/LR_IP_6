6afb6fdc55d0cd78ce5484525c2af70eb3e4ee59
gablena <57280570+gablena@users.noreply.github.com>
2021-03-23 23:53:58 +0500

    Add files via upload

---
+++ lab1_1/index.php
@@ -0,0 +1,91 @@
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="utf-8" />
+<title>Габриелян Елена</title>
+</head>
+<body>
+<table
+border="2"
+align="center"
+rules="rows"
+style="width:100%;">
+<tr>
+<td>
+<table
+border="5"
+background="images/168.png"
+bgcolor="#BDB76B"
+cellpadding="10"
+style="width:100%; border-radius:5px;">
+<tr>
+<th>
+<h1>Лабораторные работы</h1>
+<h3>Габриелян Е.В. ПИ-318</h3>
+</th>
+</tr>
+</table>
+<table
+border="1"
+bgcolor="#F0E68C"
+cellpadding="10"
+style="width:100%; border-radius:5px;">
+<tr>
+<td
+rowspan="2"
+style="width:80%">
+<h2>Общая информация</h2>
+<p style="text-indent:20px">
+Здравствуйте!
+</p>
+
+<p style="text-indent:20px"></p>
+</td>
+<td bgcolor="#EEE8AA">
+<h3>Ссылки на задания</h3>
+<p>
+<ul>
+	<li><a href="http://f0525944.xsph.ru/lab1_1.html">Задание 1</a></li>
+	<li><a href="http://f0525944.xsph.ru/lab1_2.html">Задание 2</a></li>
+    <li><a href="http://f0525944.xsph.ru/lab1_3.html">Задание 3</a></li>
+    <li><a href="http://f0525944.xsph.ru/lab1_4.html">Задание 4</a></li>
+    <li><a href="http://f0525944.xsph.ru/lab1_5.html">Задание 5</a></li>
+    <li><a href="http://f0525944.xsph.ru/lab1_6.html">Задание 6</a></li>
+    <li><a href="http://f0525944.xsph.ru/lab1_7.html">Задание 7</a></li>
+</ul>
+</p>
+</td>
+</tr>
+<tr>
+<td
+bgcolor="#F5DEB3"
+align="center">
+<h3>Общая информация</h3>
+<p>Моя почта: lena.gabrielyan29@mail.ru</p>
+</td>
+</tr>
+</table>
+<table
+border="1"
+bgcolor="#BDB76B"
+height="100"
+cellpadding="10"
+style="width:100%; border-radius:5px;">
+<tr>
+<th>
+<h3>Дата и время:</h3>
+<p> 
+<p>
+<?php
+ $d=date("d.m.Y H:i");
+ echo($d);
+?>
+</th>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</body>
+</html>
